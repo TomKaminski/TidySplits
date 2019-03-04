ddec340f2bfe45b997f11e77125110c6a36b60ac
Tomasz Kaminski <tomasz.kaminski@euvic.pl>
2019-03-04 11:08:05 +0100

    Initial Commit

---
+++ TidySplits/TidySplits.h
@@ -0,0 +1,19 @@
+//
+//  TidySplits.h
+//  TidySplits
+//
+//  Created by Tomasz Kaminski on 3/4/19.
+//  Copyright © 2019 Tomasz Kaminski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TidySplits.
+FOUNDATION_EXPORT double TidySplitsVersionNumber;
+
+//! Project version string for TidySplits.
+FOUNDATION_EXPORT const unsigned char TidySplitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TidySplits/PublicHeader.h>
+
+
