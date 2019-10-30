06d1b2d1cfca770c9673655ce1778462cfed22c1
Tomasz Kaminski <>
2019-10-30 13:57:48 +0100

    added possibility to ignore pop notifications

--- Source/Controllers/TidySplitsUITabBarViewController.swift
+++ Source/Controllers/TidySplitsUITabBarViewController.swift
@@ -11,20 +11,27 @@ import UIKit
 open class TidySplitsUITabBarViewController : UITabBarController, TidySplitsCheckpointControllerProtocol {
   public var associatedCheckpointKey: String?
   public var prefferedDisplayType: TidySplitsChildPreferedDisplayType
-  
-  public init(_ prefferedDisplayType: TidySplitsChildPreferedDisplayType) {
+  public var ignorePopNotifications: Bool
+
+  public init(_ prefferedDisplayType: TidySplitsChildPreferedDisplayType, _ ignorePopNotifications: Bool = false) {
     self.prefferedDisplayType = prefferedDisplayType
+    self.ignorePopNotifications = ignorePopNotifications
     super.init(nibName: nil, bundle: nil)
   }
   
   required public init?(coder aDecoder: NSCoder) {
     self.prefferedDisplayType = .Detail
+    self.ignorePopNotifications = false
     super.init(coder: aDecoder)
   }
   
   override open func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
     self.postPopSelfNotification()
+    
+    guard !ignorePopNotifications else {
+      return
+    }
     
     if self.isMovingFromParent {
       self.removeCheckpoint()
--- Source/Controllers/TidySplitsUITableViewController.swift
+++ Source/Controllers/TidySplitsUITableViewController.swift
@@ -11,19 +11,28 @@ import UIKit
 open class TidySplitsUITableViewController: UITableViewController, TidySplitsCheckpointControllerProtocol {
   public var associatedCheckpointKey: String?
   public var prefferedDisplayType: TidySplitsChildPreferedDisplayType
-  
-  public init(_ prefferedDisplayType: TidySplitsChildPreferedDisplayType) {
+  public var ignorePopNotifications: Bool
+
+  public init(_ prefferedDisplayType: TidySplitsChildPreferedDisplayType, _ ignorePopNotifications: Bool = false) {
     self.prefferedDisplayType = prefferedDisplayType
+    self.ignorePopNotifications = ignorePopNotifications
+
     super.init(nibName: nil, bundle: nil)
   }
   
   required public init?(coder aDecoder: NSCoder) {
     self.prefferedDisplayType = .Detail
+    self.ignorePopNotifications = false
     super.init(coder: aDecoder)
   }
   
   override open func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
+    
+    guard !ignorePopNotifications else {
+      return
+    }
+    
     self.postPopSelfNotification()
     
     if self.isMovingFromParent {
