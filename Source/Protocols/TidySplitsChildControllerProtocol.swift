33d7c6992d9119392c1ea4e8299012103fda983a
Tomasz Kaminski <tomasz.kaminski@euvic.pl>
2022-07-10 16:43:42 +0200

    parent check as nav.

--- Source/Protocols/TidySplitsChildControllerProtocol.swift
+++ Source/Protocols/TidySplitsChildControllerProtocol.swift
@@ -35,7 +35,7 @@ public extension TidySplitsChildControllerProtocol where Self: UIViewController 
   }
   
   func handleOnPopOrDismiss() {
-    if isMovingFromParent && allowAfterPopAction {
+    if isMovingFromParent && allowAfterPopAction && self.parent is TidySplitsUINavigationController {
       self.tidySplitController?.navigator.afterPop(from: self.prefferedDisplayType)
     }
   }
